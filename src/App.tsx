bfc3d8eef5d12030a8f300283dbd9ab9d8df7abb
Alexandre Piva <alexandrehpiva@gmail.com>
2021-08-09 21:10:46 -0300

    First socket.io client example in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,34 @@
-import './App.css';
+import { io } from 'socket.io-client';
+
+const sio = io('http://localhost:8000');
+
+sio.on('connect', () => {
+  console.log('connected');
+
+  // After sum_to_sum_result calc event, the sum_result event will be called
+  sio.emit('sum_to_sum_result', {
+    numbers: [1, 2],
+  });
+
+  // Use the second parameter by passing a callback to manipulate the response
+  sio.emit(
+    'sum',
+    {
+      numbers: [1, 2],
+    },
+    (sumResponse: number) => {
+      console.log(sumResponse);
+    }
+  );
+});
+
+sio.on('disconnect', () => {
+  console.log('disconnected');
+});
+
+sio.on('sum_result', sumResultResponse => {
+  console.log(sumResultResponse);
+});
 
 function App() {
   return <div className="App"></div>;
